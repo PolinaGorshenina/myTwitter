8f28f4f34a4c1796a8de814ae9874964b4ef6a89
Polina Gorshenina <polina.gorshenina02@gmail.com>
2020-05-19 15:13:53 +0300

    add task 8

---
+++ task8/Project8/src/main/java/Page.java
@@ -0,0 +1,15 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Page extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String staticPagePath = "WEB-INF/page.html";
+        RequestDispatcher view = req.getRequestDispatcher(staticPagePath);
+        view.forward(req, resp);
+    }
+}
